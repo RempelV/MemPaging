148c9ab8f8882b16cac8c717f66fb5d838602573
Vitor Rempel <vitorrempel.contato@gmail.com>
2024-06-26 00:01:26 -0300

    rascunho da logica

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ int get_params(int *physicalSizeAddr, int *pageSizeAddr, int *processMaxSizeAddr
 }
 
 int main() { 
-    int running = 1;
+    _Bool running = 1;
     int physicalSize;
     int pageSize;
     int processMaxSize;
@@ -40,20 +40,41 @@ int main() {
     - Tamanho Página: %d bytes \n\
     - Tamanho Máximo Processo: %d bytes\n\n", physicalSize, pageSize, processMaxSize);
 
+    int physicalMemory[physicalSize/pageSize];
+
+
 while (running) {
     main_menu(&menuOpt);
     switch (menuOpt) {
         case 1:
             printf("Opção selecionada: Visualizar Memória\n");
+            // Percorre memoria fisica e printa cada quadro (vazio ou valor dentro)
+            // Printa porcentagem entre livres/ocupados
             break;
         case 2:
             printf("Opção selecionada: Visualizar Tabela de Páginas\n");
+            // Percorre lista encadeada
+            // Listar id dos processos
+            // input processo
+            // Mostrar a tabela do processo
             break;
         case 3:
             printf("Opção selecionada: Criar um Processo\n");
+            int id;
+            int tamanho;
+            printf("Digite um id: ");
+            scanf("%d", &id);
+            printf("Digite um tamanho: ");
+            scanf("%d", &tamanho);
+
+            // verifica se eh maior que o tamanho maximo
+            // se sim mensagem de erro
+            // senao verifica se há disponibilidade no endereço logico
+            // se houver: adiciona um processo na lista encadeada de processos com os quadros disponiveis na lista encadeada de quadros
+            // senao mensagem de erro
             break;
         default:
-            printf("Opção selecionada: Sair. Saindo...\n", menuOpt);
+            printf("Opção selecionada: Sair. Saindo...\n");
             running = 0;
             break;
     }
